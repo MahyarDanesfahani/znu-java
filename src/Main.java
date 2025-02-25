862d93c1ec305ba14c1f37abbdeb86ef411eaf4e
mahyar <mahyardanesfahani@gmail.com>
2025-02-25 08:43:03 +0330

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("what is your name ? ");
+        String name = scanner.nextLine();
+        System.out.println("welcome " + name);
+
+        System.out.println("please enter the weight (kilograms) : ");
+        double m = scanner.nextDouble();
+        System.out.println("please enter the height (meter) : ");
+        double h = scanner.nextDouble();
+
+        earth(m,h);
+    }
+    public static void earth(double weight , double height){
+        final double g = 9.8d ;
+        double result = weight * height * g ;
+        System.out.println("the Earth's gravitational potential energy is : " + result);
     }
 }
