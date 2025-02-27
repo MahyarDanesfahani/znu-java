67baa1b1b6edf8298b25b6983ab1c1a4c63c4678
mahyar <mahyardanesfahani@gmail.com>
2025-02-27 13:47:20 +0330

    t E5.15

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,10 @@ public class Main {
         System.out.println("please enter the double number : ");
         double x = scanner.nextDouble();
         check(x);
+
+        System.out.println("please enter your income : ");
+        int income = scanner.nextInt();
+        tax(income);
     }
     public static void earth(double weight , double height){
         final double g = 9.8d ;
@@ -36,4 +40,27 @@ public class Main {
             System.out.println("the number is negative and small");
         }
     }
+    public static void tax(int income){
+        double tax ;
+        if ( income <= 50000){
+            tax = income / 100;
+            System.out.println("the tax of your income is : " + tax);
+        } else if ( income > 50000 && income <= 75000) {
+            tax = 2 *(income / 100);
+            System.out.println("the tax of your income is : " + tax);
+        } else if (income >75000 && income <= 100000) {
+            tax =  3 * (income / 100);
+            System.out.println("the tax of your income is : " + tax);
+        } else if ( income > 100000 && income <= 250000) {
+            tax = 4 * (income / 100);
+            System.out.println("the tax of your income is : " + tax);
+        } else if ( income > 250000 && income <= 500000) {
+            tax = 5 * (income / 100);
+            System.out.println("the tax of your income is : " + tax);
+        } else {
+            tax = 6 * (income / 100);
+            System.out.println("the tax of your income is : " + tax);
+        }
+    }
+
 }
