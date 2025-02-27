195be2e752a647fc17e90dcbb67c0180371f60e8
mahyar <mahyardanesfahani@gmail.com>
2025-02-27 14:19:17 +0330

    t E5.18

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,13 @@ public class Main {
         System.out.println("please enter your income : ");
         int income = scanner.nextInt();
         tax(income);
+
+        System.out.println("please enter the 3 string : ");
+        String n1 = scanner.nextLine();
+        String n2 = scanner.nextLine();
+        String n3 = scanner.nextLine();
+        sort(n3,n2,n1);
+
     }
     public static void earth(double weight , double height){
         final double g = 9.8d ;
@@ -61,6 +68,42 @@ public class Main {
             tax = 6 * (income / 100);
             System.out.println("the tax of your income is : " + tax);
         }
+    }
+    public static void sort(String n1 , String n2 , String n3){
+        String first , second , third ;
+        if (n1.compareTo(n2) <= 0 && n1.compareTo(n3) <= 0) {
+            first = n1;
+            if (n2.compareTo(n3) <= 0) {
+                second = n2;
+                third = n3;
+            } else {
+                second = n3;
+                third = n2;
+            }
+        } else if (n2.compareTo(n1) <= 0 && n2.compareTo(n3) <= 0) {
+            first = n2;
+            if (n1.compareTo(n3) <= 0) {
+                second = n1;
+                third = n3;
+            } else {
+                second = n3;
+                third = n1;
+            }
+        } else {
+            first = n3;
+            if (n1.compareTo(n2) <= 0) {
+                second = n1;
+                third = n2;
+            } else {
+                second = n2;
+                third = n1;
+            }
+        }
+        System.out.println(" the sort of string");
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
     }
 
 }
