de6a192f2a219429308766e81cef60a9364e2798
mahyar <mahyardanesfahani@gmail.com>
2025-02-27 14:53:39 +0330

    t E6.1 a.e

--- src/Main.java
+++ src/Main.java
@@ -16,20 +16,25 @@ public class Main {
         double h = scanner.nextDouble();
 
         earth(m,h);
-
+        //tE5.2
         System.out.println("please enter the double number : ");
         double x = scanner.nextDouble();
         check(x);
-
+        //E5.15
         System.out.println("please enter your income : ");
         int income = scanner.nextInt();
         tax(income);
-
+        //E5.18
         System.out.println("please enter the 3 string : ");
         String n1 = scanner.nextLine();
         String n2 = scanner.nextLine();
         String n3 = scanner.nextLine();
         sort(n3,n2,n1);
+        //E6.1 a,e
+        sum100();
+        System.out.println("please enter the int number : ");
+        int number = scanner.nextInt();
+        sumnumberodd(number);
 
     }
     public static void earth(double weight , double height){
@@ -105,5 +110,25 @@ public class Main {
         System.out.println(third);
 
     }
-
+    public static  void sum100(){
+        int sum = 0;
+        for(int i=2 ; i <= 100 ; i++){
+            if ( i % 2 == 0){
+                sum += i ;
+            }
+        }
+        System.out.println("the sum of even number between 2 and 100 : " + sum);
+    }
+    public static  int sumnumberodd(int number){
+        int s = 0 ;
+        while (number != 0) {
+            int digit = number % 10;
+            if (digit % 2 != 0) {
+                s += digit;
+            }
+            number /= 10;
+        }
+        System.out.println("the sum of odd number is : " + s);
+        return  s ;
+    }
 }
