8ea2bd4dc01e050268e1dc68051ca8d0fd3791d7
mahyar <mahyardanesfahani@gmail.com>
2025-02-27 15:15:43 +0330

    t E6.3

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,11 @@ public class Main {
         sumnumberodd(number);
         //E6.2
         processNumbers();
+        //E6.3
+        System.out.println("please enter the strings : ");
+        String input = scanner.nextLine();
+        processString(input);
+
 
     }
     public static void earth(double weight , double height){
@@ -181,6 +186,37 @@ public class Main {
         System.out.println("the All adjacent duplicates : " + (duplicatesOutput.length() > 0 ? duplicatesOutput.toString().trim() : "nothing"));
 
         scanner.close();
+    }
+    public static void processString(String input) {
+        StringBuilder uppercaseLetters = new StringBuilder();
+        StringBuilder everySecondChar = new StringBuilder();
+        StringBuilder replacedVowels = new StringBuilder();
+        StringBuilder vowelPositions = new StringBuilder();
+        int vowelCount = 0;
+        String vowels = "AEIOUaeiou";
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                uppercaseLetters.append(ch);
+            }
+            if (i % 2 == 1) {
+                everySecondChar.append(ch);
+            }
+            if (vowels.indexOf(ch) != -1) {
+                replacedVowels.append('_');
+                vowelCount++;
+                vowelPositions.append(i).append(" ");
+            } else {
+                replacedVowels.append(ch);
+            }
+        }
+        System.out.println("\na.Only the uppercase letters in the string : " + (uppercaseLetters.length() > 0 ? uppercaseLetters.toString() : "هیچ‌کدام"));
+        System.out.println("b.Every second letter of the string : " + (everySecondChar.length() > 0 ? everySecondChar.toString() : "هیچ‌کدام"));
+        System.out.println("c.the string, with all vowels replaced by an underscore : " + replacedVowels.toString());
+        System.out.println("d.the number of vowels in the string : " + vowelCount);
+        System.out.println("e.the positions of all vowels in the string : " + (vowelPositions.length() > 0 ? vowelPositions.toString().trim() : "هیچ‌کدام"));
     }
 
 }
